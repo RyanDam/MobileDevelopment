327df43e387c0eb63ca0ba10aa132f1fc75bd138
admin <dinamonle@gmail.com>
2017-05-12 12:42:14 +0700

    Get HTML source content

---
+++ app/src/main/java/com/rstudio/assmb/latie/DownloadHTML.java
@@ -0,0 +1,8 @@
+package com.rstudio.assmb.latie;
+
+/**
+ * Created by admin on 5/7/17.
+ */
+
+public class DownloadHTML {
+}
--- app/src/main/java/com/rstudio/assmb/latie/MainActivity.java
+++ app/src/main/java/com/rstudio/assmb/latie/MainActivity.java
@@ -5,23 +5,46 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.os.AsyncTaskCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.StringBuilderPrinter;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.URLUtil;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
 import com.rstudio.assmb.latie.contentfragment.ItemModelFragment;
 import com.rstudio.assmb.latie.contentfragment.dummy.DummyContent;
+import com.rstudio.assmb.latie.parser.HTMLParser;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity implements ItemModelFragment.OnListFragmentInteractionListener {
 
@@ -71,6 +94,7 @@ public class MainActivity extends AppCompatActivity implements ItemModelFragment
         receivedContent();
 
     }
+
     public void receivedContent() {
         //get the received intent
         Intent receivedIntent = getIntent();
@@ -80,12 +104,22 @@ public class MainActivity extends AppCompatActivity implements ItemModelFragment
         if(receivedAction.equals(Intent.ACTION_SEND)){
             //content is being shared
             if(receivedType.startsWith("text/")){
+                sendNotification();
                 Toast.makeText(this, "You received text!", Toast.LENGTH_SHORT).show();
                 String receivedText = receivedIntent.getStringExtra(Intent.EXTRA_TEXT);
                 if(receivedText != null) {
-                   sendNotification();
+                    String abc="";
+                    if(URLUtil.isValidUrl(receivedText)) {
+                        DownloadHTML handle = new DownloadHTML(this,receivedText);
+                        handle.execute(receivedText);
+                        Log.d("HEHEHE",abc);
+                        Toast.makeText(this, "URL", Toast.LENGTH_SHORT).show();
 
-                    Toast.makeText(this, receivedText, Toast.LENGTH_SHORT).show();
+                    }else {
+                        Toast.makeText(this, receivedText, Toast.LENGTH_SHORT).show();
+                    }
+
+
                 }
             }
         }else if(receivedAction.equals(Intent.ACTION_MAIN)){
@@ -96,8 +130,8 @@ public class MainActivity extends AppCompatActivity implements ItemModelFragment
     public void sendNotification() {
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
                         .setSmallIcon(R.drawable.ic_home_black_24dp)
-                        .setContentTitle("My notification")
-                        .setContentText("Hello World");
+                        .setContentTitle("You has received something!")
+                        .setContentText("This is your content");
         Intent notificationInetent = new Intent(this, MainActivity.class);
         notificationInetent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |  Intent.FLAG_ACTIVITY_SINGLE_TOP);
         PendingIntent contentIntent = PendingIntent.getActivity(this,0, notificationInetent,PendingIntent.FLAG_UPDATE_CURRENT);
@@ -120,4 +154,7 @@ public class MainActivity extends AppCompatActivity implements ItemModelFragment
         startActivity(intent);
     }
 
+
+
 }
+
